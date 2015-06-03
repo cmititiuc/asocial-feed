e054dd3b21e09a9fbc21247f91b295c681822543
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2015-06-03 15:53:48 -0700

    Add form to posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
+    @post = Post.new
     @posts = current_user.posts
 
     if params[:filter]
@@ -37,7 +38,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to (params[:return_to] ? params[:return_to] : @post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -78,6 +79,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body, :user_id, :topic_id)
+      params.require(:post).permit(:body, :user_id, :topic_id, :return_to)
     end
 end
