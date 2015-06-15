6deef6a979ee6e6fa5f7b393c9e129fa7074a6dc
Catalin Mititiuc <catalin.mititiuc@gmail.com>
2015-06-15 14:53:53 -0700

    Add logo to header

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -28,3 +28,22 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
       end
     end
 end
+
+# create a post
+# edit a post
+# destroy a post
+# edit link changes to cancel link
+# cancel link changes to edit link
+  # on cancel
+  # on update
+# same day date transfered on destroy
+# form injected on edit
+# correct text on injected form
+# test sign up
+# test log in as a guest
+# test formatting help
+# topic changes to none after create if no filter is set
+# topic changes to filter when filter is set
+# topic stays when post created while filter is set
+# post created with topic not matching filter is not displayed
+# post created matching filter is displayed
